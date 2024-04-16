9ef563e5726312b66c4877e5c6a753235d7e73fe
Alokananda Y <plantsfortrees@gmail.com>
2024-04-16 06:46:35 +0530

    react

--- josh-comeau/hello-react/app.js
+++ josh-comeau/hello-react/app.js
@@ -1,6 +1,13 @@
 const app = document.getElementById("app");
 
-
-
+// Create a react element
+const element = React.createElement(
+    'p',
+    { id: 'hello '},
+    'Hello World'
+);
 
 const root = ReactDOM.createRoot(app);
+
+// Render the application
+root.render(element);
