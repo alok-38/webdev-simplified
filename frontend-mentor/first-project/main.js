4c0f2b6860ffce09218e5a3e42ffb0a9bfe3b97b
Alokananda Y <plantsfortrees@gmail.com>
2024-04-25 15:41:55 +0530

    quiz app

--- frontend-mentor/first-project/main.js
+++
@@ -1,9 +0,0 @@
-const buttons = document.querySelectorAll('button');
-const divElement = document.getElementById('screen');
-
-for (let index = 0; index < buttons.length; index++) {
-    buttons[index].addEventListener('click', (e) => {
-        console.log('Button clicked!');
-        divElement.style.backgroundColor = e.target.id;
-    });
-}
--- weather-app/config.js
+++
@@ -1,2 +0,0 @@
-// config.js
-window.APIKEY = "fa00ba01b20702e72cc8eb3e082bbbca";
