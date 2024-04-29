bc0f34c6df3a23656f8e6d0a96a5c0c810c34ea1
Alokananda Y <plantsfortrees@gmail.com>
2024-04-29 13:19:44 +0530

    import Header from './Header'; import Counter from './Counter';
    const App = () => {
      return (
        <div>
          <Header />
          <Counter />
        </div>
      );
    };
    
    export default App;

---
+++ react-simplified/declarative-nature/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Counter from "./components/Counter";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
