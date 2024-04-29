d034a65fea3165d2d88a2e2b84bbc37b2f6a2d31
Alokananda Y <plantsfortrees@gmail.com>
2024-04-29 17:31:31 +0530

    user card

--- react-simplified/user-card/src/App.jsx
+++ react-simplified/user-card/src/App.jsx
@@ -1,5 +1,20 @@
-import UserCard from "./components/UserCard";
+import viteLogo from "/vite.svg";
+import { UserCard } from "./components/UserCard";
 
-export default function App() {
-  return <UserCard />;
+function App() {
+  return (
+    <div className="flex flex-col items-center">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>User Card</h1>
+      <div className="card">
+        <UserCard />
+      </div>
+    </div>
+  );
 }
+
+export default App;
