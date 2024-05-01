0fb6c2f4f5d15eda2c49ea5e80d98d779b21c9eb
Alokananda Y <plantsfortrees@gmail.com>
2024-05-01 14:14:37 +0530

    fruits

--- javaScript-simplified/fruit-manipulator/app.js
+++ javaScript-simplified/fruit-manipulator/app.js
@@ -1,40 +1,46 @@
 const inputElement = document.querySelector("input");
 const buttonElement = document.querySelector("button");
-
 const fruitsListDivElement = document.getElementById("fruits-list");
-
-// Empty array to store fruits
 let fruits = [];
 
-// Function to display fruits
 const displayFruits = () => {
-  // Clear the previous list
   fruitsListDivElement.innerHTML = "";
 
-  //   Dispay each fruit in the list
   fruits.forEach((fruit) => {
-    // Create a list item
     const listElement = document.createElement("li");
     listElement.textContent = fruit;
-    // Append this to the fruits element div
+    listElement.addEventListener("mouseover", toggleStrikeThrough); // Add event listener for hovering
+    listElement.addEventListener("click", removeFruit); // Add event listener for clicking
     fruitsListDivElement.appendChild(listElement);
   });
 };
 
-// Function to add a new fruit
 const addFruit = () => {
   const newFruit = inputElement.value.trim();
 
   if (newFruit !== "") {
-    // Add the new fruit to the array
     fruits.push(newFruit);
-    // Display the updated list of fruits
     displayFruits();
-    // clear the input field
     inputElement.value = "";
   }
 };
 
+const toggleStrikeThrough = (event) => {
+  if (event.target.tagName === "LI") {
+    if (event.type === "mouseover") {
+      event.target.classList.add("strikethrough"); // Add the class on mouseover
+    } else if (event.type === "mouseout") {
+      event.target.classList.remove("strikethrough"); // Remove the class on mouseout
+    }
+  }
+};
 
-// Event listener for the add fruit button
-buttonElement.addEventListener('click', addFruit);
+const removeFruit = (event) => {
+  if (event.target.tagName === "LI") {
+    const removedFruit = event.target.textContent;
+    fruits = fruits.filter((fruit) => fruit !== removedFruit);
+    displayFruits();
+  }
+};
+
+buttonElement.addEventListener("click", addFruit);
