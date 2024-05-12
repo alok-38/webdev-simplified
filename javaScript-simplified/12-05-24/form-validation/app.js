32474b19837e60557cee68c19dd5beb0a15955af
Alokananda Y <plantsfortrees@gmail.com>
2024-05-12 19:49:33 +0530

    form validation

--- javaScript-simplified/12-05-24/form-validation/app.js
+++ javaScript-simplified/12-05-24/form-validation/app.js
@@ -8,7 +8,7 @@ const inputTypePasswordConfirmation = document.getElementById(
   "password-confirmation"
 );
 const inputTypeCheckbox = document.querySelector("input[type='checkbox']");
-const buttonElement = document.querySelector("button");
+const formElement = document.querySelector("form");
 
 // TODO: Create an event listener for when the form is submitted and do the following inside of it.
 //    TODO: Create an array to store all error messages and clear any old error messages
@@ -29,20 +29,28 @@ const acceptFormInputs = (e) => {
   if (acceptPassword.length < 10) {
     errorContainer.push("Ensure the password is at least 10 characters long");
   }
-  if (acceptPassword.length !== inputTypePasswordConfirmation.length) {
-    errorContainer.push("Passwords does not match");
+  const acceptPasswordConfirmation = inputTypePasswordConfirmation.value.trim();
+  if (acceptPassword !== acceptPasswordConfirmation) {
+    errorContainer.push("Passwords do not match");
   }
   const acceptCheckBox = inputTypeCheckbox.checked;
   if (!acceptCheckBox) {
     errorContainer.push("Please agree to the terms by checking the box");
   }
-  inputTypeText.value = "";
-  inputTypePassword.value = "";
-  inputTypeCheckbox.checked = false; // Clear the checkbox
-  console.log(errorContainer);
+  if (errorContainer.length > 0) {
+    // If there are errors, prevent form submission
+    e.preventDefault();
+    // Display error messages
+    showErrors(errorContainer);
+  } else {
+    // If no errors, proceed with form submission
+    // Clear input values
+    inputTypeText.value = "";
+    inputTypePassword.value = "";
+    inputTypePasswordConfirmation.value = "";
+    inputTypeCheckbox.checked = false; // Clear the checkbox
+  }
 };
-
-buttonElement.addEventListener("click", acceptFormInputs);
 
 // TODO: Define this function
 function clearErrors() {
@@ -51,6 +59,10 @@ function clearErrors() {
   // I recommend using a while loop to accomplish this task
   // This is the trickiest part of this exercise so if you get stuck and are unable to progress you can also set the innerHTML property of the error-list to an empty string and that will also clear the children. I recommend trying to accomplish this with a while loop, though, for practice.
   // Also, make sure you remove the show class to the errors container
+  const errorList = document.getElementById("error-list");
+  while (errorList.firstChild) {
+    errorList.removeChild(errorList.firstChild);
+  }
 }
 
 // TODO: Define this function
@@ -58,4 +70,12 @@ function showErrors(errorMessages) {
   // Add each error to the error-list element
   // Make sure to use an li as the element for each error
   // Also, make sure you add the show class to the errors container
+  const errorsList = document.getElementById("errors-list");
+  errorMessages.forEach((errorMessage) => {
+    const liElement = document.createElement("li");
+    liElement.textContent = errorMessage;
+    errorsList.appendChild(liElement);
+  });
 }
+
+formElement.addEventListener("submit", acceptFormInputs);
