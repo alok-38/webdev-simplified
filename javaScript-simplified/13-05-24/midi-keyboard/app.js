dde75f68876a0903c4b3210ca48224ca3c260e8a
Alokananda Y <plantsfortrees@gmail.com>
2024-05-13 07:08:12 +0530

    Midi Piano

--- javaScript-simplified/13-05-24/midi-keyboard/app.js
+++ javaScript-simplified/13-05-24/midi-keyboard/app.js
@@ -1,3 +1,5 @@
+const audioContext = new AudioContext();
+
 const NOTE_DETAILS = [
   { note: "C", key: "Z", frequency: 261.626, active: false },
   { note: "Db", key: "S", frequency: 277.183, active: false },
@@ -17,20 +19,51 @@ document.addEventListener("keydown", (e) => {
   if (e.repeat) return;
   const keyboardKey = e.code;
   const noteDetail = getNoteDetail(keyboardKey);
+
   if (noteDetail == null) return;
+
   noteDetail.active = true;
   playNotes();
 });
 
 document.addEventListener("keyup", (e) => {
-  console.log("Up");
-  console.log(e);
+  const keyboardKey = e.code;
+  const noteDetail = getNoteDetail(keyboardKey);
+
+  if (noteDetail == null) return;
+
+  noteDetail.active = false;
+  playNotes();
 });
 
 function getNoteDetail(keyboardKey) {
-  return NOTE_DETAILS.find((n) => `key${n.key}` === keyboardKey);
+  return NOTE_DETAILS.find((n) => `Key${n.key}` === keyboardKey);
 }
 
 function playNotes() {
-  console.log("play notes");
+  NOTE_DETAILS.forEach((n) => {
+    const keyElement = document.querySelector(`[data-note="${n.note}"]`);
+    keyElement.classList.toggle("active", n.active);
+    if (n.oscillator != null) {
+      n.oscillator.stop();
+      n.oscillator.disconnect();
+    }
+  });
+
+  const activeNotes = NOTE_DETAILS.filter((n) => n.active);
+  const gain = 1 / activeNotes.length;
+  activeNotes.forEach((n) => {
+    startNote(n, gain);
+  });
+}
+
+function startNote(noteDetail, gain) {
+  const gainNode = audioContext.createGain();
+  gainNode.gain.value = gain;
+  const oscillator = audioContext.createOscillator();
+  oscillator.frequency.value = noteDetail.frequency;
+  oscillator.type = "sine";
+  oscillator.connect(gainNode).connect(audioContext.destination);
+  oscillator.start();
+  noteDetail.oscillator = oscillator;
 }
