387e5b359dcc437dd03409da508558a657ab00d5
Alokananda Y <plantsfortrees@gmail.com>
2024-05-13 06:54:28 +0530

    Midi Piano

---
+++ javaScript-simplified/13-05-24/midi-keyboard/app.js
@@ -0,0 +1,36 @@
+const NOTE_DETAILS = [
+  { note: "C", key: "Z", frequency: 261.626, active: false },
+  { note: "Db", key: "S", frequency: 277.183, active: false },
+  { note: "D", key: "X", frequency: 293.665, active: false },
+  { note: "Eb", key: "D", frequency: 311.127, active: false },
+  { note: "E", key: "C", frequency: 329.628, active: false },
+  { note: "F", key: "V", frequency: 349.228, active: false },
+  { note: "Gb", key: "G", frequency: 369.994, active: false },
+  { note: "G", key: "B", frequency: 391.995, active: false },
+  { note: "Ab", key: "H", frequency: 415.305, active: false },
+  { note: "A", key: "N", frequency: 440, active: false },
+  { note: "Bb", key: "J", frequency: 466.164, active: false },
+  { note: "B", key: "M", frequency: 493.883, active: false },
+];
+
+document.addEventListener("keydown", (e) => {
+  if (e.repeat) return;
+  const keyboardKey = e.code;
+  const noteDetail = getNoteDetail(keyboardKey);
+  if (noteDetail == null) return;
+  noteDetail.active = true;
+  playNotes();
+});
+
+document.addEventListener("keyup", (e) => {
+  console.log("Up");
+  console.log(e);
+});
+
+function getNoteDetail(keyboardKey) {
+  return NOTE_DETAILS.find((n) => `key${n.key}` === keyboardKey);
+}
+
+function playNotes() {
+  console.log("play notes");
+}
