e75473b49622d4776ef4f0cc093c7944aa34d5a9
Alokananda Y <plantsfortrees@gmail.com>
2024-07-04 06:44:23 +0530

    Components

--- todo-next-app/src/app/page.tsx
+++ todo-next-app/src/app/page.tsx
@@ -1,10 +1,19 @@
+import AddTodo from "@/components/todos/AddTodo";
+
 export default function Home() {
   return (
     <div className="w-screen py-20 flex flex-col items-center justify-center text-center">
-      <span className="text-4xl font-extrabold uppercase block mb-5">Todo App</span>
+      <span className="text-4xl font-extrabold uppercase block mb-5">
+        Todo App
+      </span>
       <h1 className="text-5xl font-extrabold uppercase mb-5">
         <span className="lowercase">w/</span>Server Actions
       </h1>
+      <div className="flex justify-center flex-col items-center">
+        {/* input todo */}
+        <AddTodo />
+        {/* map todos */}
+      </div>
     </div>
   );
 }
